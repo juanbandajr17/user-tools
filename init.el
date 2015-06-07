f6f5a45f18677841ca659fffe2ab481468cfbd57
Juan Banda <jubajr17@gmail.com>
2015-06-07 16:14:22 -0700

    auto-indent for web modes

--- init.el
+++ init.el
@@ -93,6 +93,7 @@
 (setq web-mode-code-indent-offset 2)
 (setq web-mode-attr-indent-offset 2)
 (setq web-mode-enable-auto-closing t)
+(setq web-mode-enable-auto-indentation t)
 ;; (setq web-mode-enable-auto-pairing t)
 
 ;; projectile
