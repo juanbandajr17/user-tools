c1a1e8afe740eb1cb3f341a52ce2e8cd072eca4a
Juan Banda <jubajr17@gmail.com>
2015-06-07 01:39:37 -0700

    commented conflicting setting

--- init.el
+++ init.el
@@ -93,7 +93,7 @@
 (setq web-mode-code-indent-offset 2)
 (setq web-mode-attr-indent-offset 2)
 (setq web-mode-enable-auto-closing t)
-(setq web-mode-enable-auto-pairing t)
+;; (setq web-mode-enable-auto-pairing t)
 
 ;; projectile
 (projectile-global-mode)
