f78231219e9274c5398d57d8da6adcb2231cd90b
Juan Banda <jbanda@spokeo.com>
2015-09-30 12:47:08 -0700

    remove scroll-bar on graphic

--- init.el
+++ init.el
@@ -172,7 +172,8 @@
 (if (display-graphic-p)
     (progn
       (fringe-mode -1)
-      (tool-bar-mode -1))
+      (tool-bar-mode -1)
+      (scroll-bar-mode -1))
   (progn (menu-bar-mode -1)
          ;; (load-theme 'flatland-black)
          ))
