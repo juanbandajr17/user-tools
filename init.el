5be53cd1a052d676dad523df855c4341620dda1f
Juan Banda Jr <jubajr17@gmail.com>
2016-04-14 02:06:14 -0700

    customize settings segregated

--- init.el
+++ init.el
@@ -13,19 +13,10 @@
 ;; TERM=xterm-256color
 ;; TERM=xterm
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;; Moved the custom.el stuff into its own file called ~/.emacs.d/customize.el
+(setq custom-file "~/.emacs.d/customize.el")
+(when (file-exists-p custom-file)
+  (load custom-file))
 
 (when (>= emacs-major-version 24)
   (require 'package)
