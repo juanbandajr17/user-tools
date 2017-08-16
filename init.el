3095f5cbe2ae05f2f659f7e660dbf1ac38ed8e6f
Juan Banda <jbanda@spokeo.com>
2017-08-16 11:16:23 -0700

    query better replace

--- init.el
+++ init.el
@@ -63,6 +63,7 @@
 (global-set-key (kbd "C-r") 'isearch-backward-regexp)
 (global-set-key (kbd "C-M-s") 'isearch-forward)
 (global-set-key (kbd "C-M-r") 'isearch-backward)
+(global-set-key (kbd "M-%") 'query-replace-regexp)
 
 ;; Org
 (global-set-key "\C-cl" 'org-store-link)
