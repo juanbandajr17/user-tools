d7a515c287f7ac93e03542e5bcb668ac7b82aa06
juan <jubajr17@gmail.com>
2018-05-20 17:03:47 -0700

    tabs settings

--- init.el
+++ init.el
@@ -35,6 +35,8 @@
       ring-bell-function 'ignore)
 
 (setq-default indent-tabs-mode nil)
+(setq-default tab-width 4)
+(setq indent-line-function 'insert-tab)
 
 ;; Modes
 (column-number-mode t)  ;; Show col position in mode-line
