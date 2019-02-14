20c48a07764c8cffe6422505ff06b9a526630ba9
Juan Banda <jbanda@spokeo.com>
2019-02-14 10:33:36 -0800

    added back changes needed

--- init.el
+++ init.el
@@ -131,6 +131,11 @@
  org-agenda-skip-scheduled-if-done t
  org-agenda-skip-deadline-if-done t)
 
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-cc" 'org-capture)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cb" 'org-iswitchb)
+
 
 ;; Functions
 (defun force-backup-of-buffer ()
