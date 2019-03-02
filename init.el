ebd0639e81d4aa0378765f074955203f3c122ee6
juan <jubajr17@gmail.com>
2019-03-02 12:07:48 -0800

    updated auto-package

--- init.el
+++ init.el
@@ -29,10 +29,11 @@
 
 (use-package auto-package-update
   :ensure t
-  :config
-  (setq auto-package-update-delete-old-versions t)
-  (setq auto-package-update-hide-results t)
-  (auto-package-update-maybe))
+  :custom ((auto-package-update-interval (* 7 4))
+           (auto-package-update-delete-old-versions t)
+           (auto-package-update-hide-results t)
+           (auto-package-update-maybe)
+           (auto-package-update-prompt-before-update t)))
 
 (use-package apropos
   :custom (apropos-do-all t))
