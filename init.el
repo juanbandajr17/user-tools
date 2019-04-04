df3f5c6281eda54d9ff2de745fdabf36fe51183e
juan <jubajr17@gmail.com>
2019-04-03 18:49:04 -0700

    updated configs

--- init.el
+++ init.el
@@ -362,7 +362,8 @@
   :custom
   (web-mode-markup-indent-offset 2)
   (web-mode-css-indent-offset 2)
-  (web-mode-code-indent-offset 2))
+  (web-mode-code-indent-offset 2)
+  (web-mode-enable-current-element-highlight t))
 
 (use-package zone
   :disabled
